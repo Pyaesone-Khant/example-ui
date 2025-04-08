7809a97abe418f71a88a40d81736a6c72fb2b34e
Pyaesone Khant <pyaesonekhant.dev@gmail.com>
2025-04-08 23:55:31 +0630

    feat: add real-time room chat

--- components/WebSocket/Messages.tsx
+++ components/WebSocket/Messages.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/utils";
 import { useEffect, useRef } from "react";
 
-export function Messages({ messages }: { messages: { text: string; sender: "server" | "you" }[] }) {
+export function Messages({ messages, username }: { messages: { text: string; sender: string }[], username: string }) {
 
     const messageEndRef = useRef<HTMLDivElement | null>(null);
     useEffect(() => {
@@ -9,15 +9,29 @@ export function Messages({ messages }: { messages: { text: string; sender: "serv
     }, [messages]);
 
     return (
-        <div className="h-[300px] overflow-y-auto border border-gray-300 p-4 rounded bg-gray-100">
+        <div className="h-[500px] overflow-y-auto border border-gray-300 p-4 rounded bg-gray-100">
             {messages.map((msg, index) => (
                 <div
                     key={index}
-                    className={cn("mb-2 p-2 rounded-md max-w-[240px] bg-gray-300 text-black", {
-                        "bg-blue-500 text-white ml-auto": msg.sender === "you",
+                    className={cn("mb-2 p-2 rounded-md max-w-xs bg-gray-300 text-black", {
+                        "bg-blue-500 text-white ml-auto": msg.sender === username,
+                        "bg-black text-white text-center rounded-full mx-auto": msg.sender === "server",
                     })}
                 >
-                    {msg.text}
+                    <article
+                        className="space-y-0.5"
+                    >
+                        {
+                            msg.sender !== "server" && (
+                                <h3
+                                    className="text-sm font-semibold"
+                                >
+                                    {msg.sender === username ? "You" : msg.sender}:
+                                </h3>
+                            )
+                        }
+                        <p>{msg.text}</p>
+                    </article>
                 </div>
             ))}
             <div ref={messageEndRef}></div>
